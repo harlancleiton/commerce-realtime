940b8e1e32c2fe74f4c90da57bcc948503bda0c0
Harlan Cleiton <harlancleiton@gmail.com>
2019-09-06 23:56:14 -0300

    Add Coupon migration

---
+++ database/migrations/1567823943732_coupon_schema.js
@@ -0,0 +1,41 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CouponSchema extends Schema {
+  up() {
+    this.create('coupons', table => {
+      table.increments()
+      table
+        .string('code', 100)
+        .notNullable()
+        .unique()
+      table.datetime('valid_from')
+      table.datetime('valid_until')
+      table
+        .integer('quantity')
+        .notNullable()
+        .defaultTo(1)
+      table
+        .enu('can_use_for', ['PRODUCT', 'CLIENT', 'PRODUCT_CLIENT', 'ALL'])
+        .notNullable()
+        .defaultTo('ALL')
+      table
+        .enu('type', ['FREE', 'PERCENT', 'CURRENCY'])
+        .notNullable()
+        .defaultTo('CURRENCY')
+      table
+        .boolean('recursive')
+        .notNullable()
+        .defaultTo(false)
+      table.timestamps()
+    })
+  }
+
+  down() {
+    this.drop('coupons')
+  }
+}
+
+module.exports = CouponSchema
