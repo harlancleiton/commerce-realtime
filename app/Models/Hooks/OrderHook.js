b6395a641a855512990d8583345fb3537ff221b5
Harlan Cleiton <harlancleiton@gmail.com>
2019-09-07 11:53:42 -0300

    Add OrderHook

---
+++ app/Models/Hooks/OrderHook.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const OrderHook = (exports = module.exports = {})
+
+OrderHook.updateValues = async modelInstance => {
+  modelInstance.$sideLoaded.subtotal = await modelInstance
+    .items()
+    .getSum('subtotal')
+  modelInstance.$sideLoaded.quantity_items = await modelInstance
+    .items()
+    .getSum('quantity')
+  modelInstance.$sideLoaded.discount = await modelInstance
+    .discounts()
+    .getSum('discount')
+  modelInstance.total =
+    modelInstance.$sideLoaded.subtotal - modelInstance.$sideLoaded.discount
+}
+
+OrderHook.updateCollectionValues = async modelsInstance => {
+  for (let modelInstance of modelsInstance) {
+    modelInstance = await OrderHook.updateValues(modelInstance)
+  }
+}
