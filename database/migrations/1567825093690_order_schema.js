6da6d94e3592a06bd328aff5ef8e245aaff48e72
Harlan Cleiton <harlancleiton@gmail.com>
2019-09-07 00:12:58 -0300

    Add Order and CouponOrder migrations

---
+++ database/migrations/1567825093690_order_schema.js
@@ -0,0 +1,30 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class OrderSchema extends Schema {
+  up() {
+    this.create('orders', table => {
+      table.increments()
+      table.decimal('total', 12, 2).notNullable()
+      table.integer('user_id').unsigned()
+      table
+        .foreign('user_id')
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+      table
+        .enu('status', ['PENDING', 'CANCELLED', 'SHIPPED', 'PAID', 'FINISHED'])
+        .notNullable()
+        .defaultTo('PENDING')
+      table.timestamps()
+    })
+  }
+
+  down() {
+    this.drop('orders')
+  }
+}
+
+module.exports = OrderSchema
