549ce5a944758358d850e1faad61654165466ca7
Harlan Cleiton <harlancleiton@gmail.com>
2019-09-07 00:19:50 -0300

    Add OrderItem migration

---
+++ database/migrations/1567826039994_order_item_schema.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class OrderItemSchema extends Schema {
+  up() {
+    this.create('order_items', table => {
+      table.increments()
+      table.integer('product_id').unsigned()
+      table
+        .foreign('product_id')
+        .references('id')
+        .inTable('products')
+        .onDelete('CASCADE')
+      table
+        .integer('quantity')
+        .unsigned()
+        .notNullable()
+        .defaultTo(1)
+      table.decimal('subtotal', 12, 2).notNullable()
+      table.timestamps()
+    })
+  }
+
+  down() {
+    this.drop('order_items')
+  }
+}
+
+module.exports = OrderItemSchema
