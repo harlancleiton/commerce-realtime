d7f7e3359f6ea945ff6154c4c82132d42236d639
Harlan Cleiton <harlancleiton@gmail.com>
2019-09-08 19:36:01 -0300

    ManageMultipleUpload and ManageSingleUpload helpers

--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -22,4 +22,41 @@ const generateHash = async (length = 10) => {
   return hash
 }
 
-module.exports = { generateHash }
+/**
+ * Move single file for specified path
+ * @param {FileJar} file
+ * @param {string} path
+ * @return {FileJar}
+ */
+const manageSingleUpload = async (file, path = null) => {
+  path = path ? path : Helpers.publicPath('uploads')
+  const randomName = await generateHash(30)
+  let fileName = `${Date.now()}-${randomName}.${file.subtype}`
+  await file.move(path, { name: fileName })
+  return file
+}
+
+/**
+ * Move multiple files for speecified path
+ * @param {FileJar} files
+ * @param {string} path
+ * @return {Object}
+ */
+const manageMultipleUpload = async (fileJar, path = null) => {
+  path = path ? path : Helpers.publicPath('uploads')
+  let successes = []
+  let errors = []
+  await Promise.all(
+    fileJar.files.map(async file => {
+      //TODO file = await generateSingleUpload
+      const randomName = await generateHash(30)
+      let fileName = `${Date.now()}-${randomName}.${file.subtype}`
+      await file.move(path, { name: fileName })
+      if (file.moved()) successes.push(file)
+      else errors.push(file)
+    })
+  )
+  return { successes, errors }
+}
+
+module.exports = { generateHash, manageSingleUpload, manageMultipleUpload }
