946ab9a8881bbba510948026d9e9f477464bdfe7
Harlan Cleiton <harlancleiton@gmail.com>
2019-09-07 22:43:12 -0300

    Added RoleSeeder and ClientSeeder

---
+++ database/seeds/001RoleSeeder.js
@@ -0,0 +1,38 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| RoleSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+/** @type {import('adonis-acl/src/Models/Role')} */
+const Role = use('Role')
+
+class RoleSeeder {
+  async run() {
+    await Role.create({
+      name: 'Admin',
+      slug: 'admin',
+      description: 'System Administrator'
+    })
+    await Role.create({
+      name: 'Manager',
+      slug: 'manager',
+      description: 'Store Manager'
+    })
+    await Role.create({
+      name: 'Client',
+      slug: 'client',
+      description: 'Store Client'
+    })
+  }
+}
+
+module.exports = RoleSeeder
---
+++ database/seeds/002ClientSeeder.js
@@ -0,0 +1,38 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| ClientSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+/** @type {import('adonis-acl/src/Models/Role')} */
+const Role = use('Role')
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const User = use('App/Models/User')
+
+class ClientSeeder {
+  async run() {
+    const clientRole = await Role.findBy('slug', 'client')
+    const clients = await Factory.model('App/Models/User').createMany(30)
+    await Promise.all(
+      clients.map(async client => await client.roles().attach([clientRole.id]))
+    )
+    const admin = await User.create({
+      name: 'Harlan',
+      surname: 'Cleiton',
+      email: 'harlancleiton@gmmail.com',
+      password: 'password'
+    })
+    const adminRole = await Role.findBy('slug', 'admin')
+    await admin.roles().attach([adminRole.id])
+  }
+}
+
+module.exports = ClientSeeder
