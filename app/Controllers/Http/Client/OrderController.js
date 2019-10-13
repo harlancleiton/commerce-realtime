7a49522e8c203f9a1756b512409a75e8e5e3809b
Harlan Cleiton <harlancleiton@gmail.com>
2019-10-13 00:55:15 -0300

    OrderController.store

--- app/Controllers/Http/Client/OrderController.js
+++ app/Controllers/Http/Client/OrderController.js
@@ -2,7 +2,17 @@
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
+/** @typedef {import('@adonisjs/auth/src/Schemes/Session')} AuthSession */
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Order = use('App/Models/Order')
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Coupon = use('App/Models/Coupon')
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Discount = use('App/Models/Discount')
+const Database = use('Database')
+const Service = use('App/Services/Order/OrderService')
+const Transformer = use('App/Transformers/Admin/OrderTransformer')
+const Ws = use('Ws')
 
 /**
  * Resourceful controller for interacting with orders
@@ -15,8 +25,23 @@ class OrderController {
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
+   * @param {AuthSession} ctx.auth
+   * @param {Object} ctx.transform
+   * @param {Object} ctx.pagination
    */
-  // async index({ request, response }) {}
+  async index({ request, response, auth, transform, pagination }) {
+    const user = await auth.getUser()
+    const { number } = request.all()
+    const query = Order.query()
+    query.where('user_id', user.id)
+    if (number) query.where('id', 'ILIKE', `%${number}%`)
+    let orders = await query
+      .orderBy('id', 'desc')
+      .paginate(pagination.page, pagination.limit)
+    orders = await transform.paginate(orders, Transformer)
+    return response.send({ data: orders })
+  }
+
   /**
    * Create/save a new order.
    * POST orders
@@ -24,8 +49,54 @@ class OrderController {
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
+   * @param {AuthSession} ctx.auth
+   * @param {Object} ctx.transform
    */
-  // async store({ request, response }) {}
+  async store({ request, response, auth, transform }) {
+    const user = await auth.getUser()
+    const { items } = request.all()
+    const trx = await Database.beginTransaction()
+    try {
+      let order = await Order.create({ user_id: user.id }, trx)
+      if (items.length > 0) {
+        const service = new Service(order, trx)
+        service.syncItems(items)
+      }
+      await trx.commit()
+      // TODO order.reload() ?
+      order = await Order.find(order.id)
+      order = await transform.include('items').item(order, Transformer)
+      Ws.getChannel('notifications')
+        .topic('notifications')
+        .broadcast('new:order', order)
+      return response.created({ data: order })
+    } catch (error) {
+      await trx.rollback()
+      return response.status(500).json({
+        errors: [{ message: 'Não foi possível armazenar seu pedido' }]
+      })
+    }
+  }
+
+  /**
+   * Display a single order.
+   * GET orders/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {AuthSession} ctx.auth
+   * @param {Object} ctx.transform
+   */
+  async show({ params, response, auth, transform }) {
+    const user = await auth.getUser()
+    const { id } = params
+    let order = await Order.findOrFail(id)
+    if (order.user_id !== user.id) return response.status(403).send()
+    order = await transform.include('items,discounts').item(order, Transformer)
+    return response.send({ data: order })
+  }
+
   /**
    * Update order details.
    * PUT or PATCH orders/:id
